af8517c003b2562d26ac2a6c3f1d3138e7006b32
dontaskwyy <gasmi.ghassen@gmail.com>
2022-09-02 00:28:31 +0100

    tri

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -8,6 +8,18 @@ function Home() {
         <h3>I'm a</h3>
         <h1>FULLSTACK</h1>
         <h2>DEVELOPER</h2>
+        <div className="triangle">
+          <div></div>
+        </div>
+        <div className="triangle-1">
+          <div></div>
+        </div>
+        <div className="triangle-2">
+          <div></div>
+        </div>
+        <div className="triangle-3">
+          <div></div>
+        </div>
       </div>
     </div>
   )
